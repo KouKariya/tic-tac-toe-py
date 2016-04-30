8fe92556ab5480d05466eb695eb104c29380d1c0
KouKariya <KouKariya@users.noreply.github.com>
2016-04-30 10:36:03 -0500

    Update ticTacToe.py
    Added more functionality to pre existing functions.

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,22 +1,29 @@
 #ticTacToe.py
 
+gameGrid = [ [' ' for i in range(3)] for j in range(3)]
+gameWon = False
+gameOver = False
 
 def gameRules():
     print'Whoever matches three of a kind wins! Place either an X or an O within the grid'
 
 def printGrid():
-    print'|1|2|3|'
+    print'|'+gameGrid[0][0]+'|'+gameGrid[0][1]+'|'+gameGrid[0][2]+'|'
     print'-------'
-    print'|4|5|6|'
+    print'|'+gameGrid[1][0]+'|'+gameGrid[1][1]+'|'+gameGrid[1][2]+'|'
     print'-------'
-    print'|7|8|9|'
+    print'|'+gameGrid[2][0]+'|'+gameGrid[2][1]+'|'+gameGrid[2][2]+'|'
 
-def placeMove( move , index):
+def placeMove(move,index):
+    print move
+    x = move
+    global gameGrid
     count = 1
     for i in range(3):
         for j in range(3):
             if(count == index):
-                gameGrid[i][j] = move #add global keyword after gameGrid has been made globally
+                print move
+                gameGrid[i][j] = x
             else:
                 count = count + 1
 
@@ -28,11 +35,15 @@ def placeMove( move , index):
 
 def main():
     gameRules()
-    printGrid()
+    #printGrid()
+    while(gameWon != True or gameOver != True):
+        printGrid()
+        a = raw_input('Xs or Os?')
+        b = raw_input('Where?...')
+        placeMove(a,b)
+        gameGrid[2][2] = 'x' #TEST
 
     #create this in function and make it global
-    gameGrid = [ ['' for i in range(3)] for j in range(3)]
-    print gameGrid
     
     
     
