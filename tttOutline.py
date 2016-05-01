6ccfa6b9539e07c3339acfe5cbbecaf4ebf878bd
KouKariya <jssgallarzo@gmail.com>
2016-05-01 10:37:13 -0500

    Moved finalized script into different directory. Created a 'shell script' that the students will use to work on their own versions of ticTacToe.py

---
+++ tttOutline.py
@@ -0,0 +1,24 @@
+#tttOutline.py
+#by Jesse Gallarzo
+#When working with this file, make sure that it is renamed as 'ticTacToe.py'
+
+#gameGrid
+#gameOver
+#answer
+
+#def gameRules():
+    #Fill in code here
+
+#def printGrid():
+    #Fill in code here
+
+#def placeMove():
+    #Fill in code here
+
+#def gameWinner():
+    #Fill in code here
+
+#def main():
+    #fill in code here
+
+#main()
