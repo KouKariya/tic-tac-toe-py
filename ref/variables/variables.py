1fca2d0796e34cb31575c0af107f3c8012752791
KouKariya <jssgallarzo@gmail.com>
2016-05-24 09:30:29 -0500

    Updated comments under variables.py

--- ref/variables/variables.py
+++ ref/variables/variables.py
@@ -1,10 +1,18 @@
+#variables.py
+#written by Jesse Gallarzo
+
+#Integer variables are initialized below.
+#The print statement shows the addition of the two values.
 x = 5
 y = 6
 print(x+y)
 
+#String variables are initialized below.
+#The print statement shows the concantenation of the two values.
 x = '5'
 y = '6'
 print(x+y)
 
-name = input()
+#The variable 'name' is prompting input from the user by asking what its name is.
+name = input('What is your name? ')
 print('Hello'+name+'nice to meet you!')
