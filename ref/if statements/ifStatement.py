e0134b9e575b9a35592a8f47678c77e81e6db532
KouKariya <jssgallarzo@gmail.com>
2016-05-26 02:12:34 -0500

    Created the ifStatement script in order to demonstrate the use of conditional statements. In this script, the user is prompted for their age. When a value is given, it compares the value to the value within the if statement. Returns the appropriate statement depending on the value.

---
+++ ref/if statements/ifStatement.py
@@ -0,0 +1,11 @@
+#ifStatement.py
+#written by Jesse Gallarzo
+
+#The script prompts the user for their age.
+age = int(input('How old are you? '))
+
+#The if statement compares the age against a predetermined value
+if(age > 17):
+    print('You are an adult, congradulations...now get a job!')
+else:
+    print('You are still a kid...now go to school!')
