4652414722a64dcf778753f5d4bd7fd6241ef315
KouKariya <KouKariya@users.noreply.github.com>
2016-05-27 19:12:44 -0500

    Update ifStatement.py

--- ref/if statements/ifStatement.py
+++ ref/if statements/ifStatement.py
@@ -6,6 +6,8 @@ age = int(input('How old are you? '))
 
 #The if statement compares the age against a predetermined value
 if(age > 17):
+    #If the age recorded is above 17...
     print('You are an adult, congradulations...now get a job!')
+    #If the age recorded is 17 or below...
 else:
     print('You are still a kid...now go to school!')
