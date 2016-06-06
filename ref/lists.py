da7748fdb385d0d17156530dcc36321ae9a14b07
KouKariya <jssgallarzo@gmail.com>
2016-06-05 22:09:38 -0500

    Moved the contents of lists.py into the lists folder and updated the script.

--- ref/lists.py
+++
@@ -1,7 +0,0 @@
-#lists.my
-#Written by Jesse Gallarzo
-
-#populate code here
-#check spacing
-
-#add multi list example below
---
+++ ref/lists/lists.py
@@ -0,0 +1,29 @@
+#lists.my
+#Written by Jesse Gallarzo
+
+listOfNames = []
+
+#A for loop that runs five times.
+#For every iteration, the name variable gets added to the list until the loop stops.
+for num in range(5):
+    name = input('Enter a name: ')
+    listOfNames.append(name)
+
+print(listOfNames)
+
+#For every value(num) within the list 'listOfNames'
+#print out a specific value from the list in the index denoted by 'num'.
+for num in range(len(listOfNames)):
+    print(listOfNames[num])
+
+#Below is a 3 dimentional list.
+#The 3x3 list serves as an example to demonstrate how to create lists within lists, how to iterate through a 3 dimentional list
+# and how to print out its content.
+threeThreeGrid = [[' ' for i in range(3)] for j in range(3)]
+
+for i in range(3):
+    for j in range(3):
+            name = input('Enter a name: ')
+            threeThreeGrid[i][j] = name
+
+print(threeThreeGrid)
